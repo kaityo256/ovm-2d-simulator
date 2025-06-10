243be7c670fe65a10d8abe7f26c205232c6733b1
H. Watanabe <kaityo@users.sourceforge.jp>
2025-06-10 17:36:46 +0900

    :memo: updates

--- main.cpp
+++ main.cpp
@@ -8,16 +8,16 @@
 #include <vector>
 
 // パラメータ
-const double alpha = 0.25;
-const double beta = 2.5;
-const double a = 0.5;
-const double b = 1.0;
-const double c = -1.0;
-const double dt = 0.5; // 時間刻み
-const double R = 2.0;  // Search Length
-
-// システムサイズ
-double LX, LY;
+struct SimulationInfo {
+  double LX, LY;
+  const double alpha = 0.25;
+  const double beta = 2.5;
+  const double a = 0.5;
+  const double b = 1.0;
+  const double c = -1.0;
+  const double dt = 0.5; // 時間刻み
+  const double R = 2.0;  // Search Length
+};
 
 // 歩行者クラス
 struct People {
@@ -28,7 +28,7 @@ struct People {
   std::vector<int> neighbors;
 };
 
-void calculate_force(int j, int k, std::vector<People> &agents) {
+void calculate_force(int j, int k, std::vector<People> &agents, SimulationInfo &si) {
   auto &pj = agents[j];
   const auto &pk = agents[k];
 
@@ -36,10 +36,10 @@ void calculate_force(int j, int k, std::vector<People> &agents) {
   double dx = pk.x - pj.x;
   double dy = pk.y - pj.y;
 
-  if (dx < -LX * 0.5) dx += LX;
-  if (dx > LX * 0.5) dx -= LX;
-  if (dy < -LY * 0.5) dy += LY;
-  if (dy > LY * 0.5) dy -= LY;
+  if (dx < -si.LX * 0.5) dx += si.LX;
+  if (dx > si.LX * 0.5) dx -= si.LX;
+  if (dy < -si.LY * 0.5) dy += si.LY;
+  if (dy > si.LY * 0.5) dy -= si.LY;
 
   double r = std::sqrt(dx * dx + dy * dy);
   double v0_norm = std::sqrt(pj.v0_x * pj.v0_x + pj.v0_y * pj.v0_y);
@@ -50,7 +50,7 @@ void calculate_force(int j, int k, std::vector<People> &agents) {
   double n_x = dx / r;
   double n_y = dy / r;
 
-  double force_magnitude = alpha * (std::tanh(beta * (r - b)) + c);
+  double force_magnitude = si.alpha * (std::tanh(si.beta * (r - si.b)) + si.c);
   // 全体の力積ベクトル: dt * f(r_kj) * (1 + cos_phi) * n_kj
   double fx = force_magnitude * (1.0 + cos_phi) * n_x;
   double fy = force_magnitude * (1.0 + cos_phi) * n_y;
@@ -61,9 +61,9 @@ void calculate_force(int j, int k, std::vector<People> &agents) {
 }
 
 // 隣接歩行者を探す
-void update_neighbors(std::vector<People> &agents) {
+void update_neighbors(std::vector<People> &agents, SimulationInfo &si) {
   const int N = agents.size();
-  const double R2 = R * R;
+  const double R2 = si.R * si.R;
 
   for (auto &p : agents) {
     p.neighbors.clear();
@@ -73,10 +73,10 @@ void update_neighbors(std::vector<People> &agents) {
     for (int k = j + 1; k < N; ++k) {
       double dx = agents[k].x - agents[j].x;
       double dy = agents[k].y - agents[j].y;
-      if (dx < -LX * 0.5) dx += LX;
-      if (dy < -LY * 0.5) dy += LY;
-      if (dx > LX * 0.5) dx -= LX;
-      if (dy > LY * 0.5) dy -= LY;
+      if (dx < -si.LX * 0.5) dx += si.LX;
+      if (dy < -si.LY * 0.5) dy += si.LY;
+      if (dx > si.LX * 0.5) dx -= si.LX;
+      if (dy > si.LY * 0.5) dy -= si.LY;
 
       double dist2 = dx * dx + dy * dy;
 
@@ -89,7 +89,7 @@ void update_neighbors(std::vector<People> &agents) {
 }
 
 void initialize_agents_triangular_lattice(std::vector<People> &agents,
-                                          double &LX, double &LY,
+                                          SimulationInfo &si,
                                           const double r,
                                           std::mt19937 &rng) {
   const int nx = 20;
@@ -98,8 +98,8 @@ void initialize_agents_triangular_lattice(std::vector<People> &agents,
   const double dx = r;
   const double dy = std::sqrt(3.0) / 2.0 * r;
 
-  LX = nx * dx;
-  LY = ny * dy;
+  si.LX = nx * dx;
+  si.LY = ny * dy;
 
   const int N = nx * ny;
   agents.clear();
@@ -152,41 +152,40 @@ void write_agent_frame(const std::vector<People> &agents) {
   ++index;
 }
 
-void step_simulation(std::vector<People> &agents) {
-
+void step_simulation(std::vector<People> &agents, SimulationInfo &si) {
   for (auto &p : agents) {
     p.fx = 0.0;
     p.fy = 0.0;
   }
 
   // 近傍リストの更新
-  update_neighbors(agents);
+  update_neighbors(agents, si);
 
   // jが受ける力を各kから計算して積算
   for (int j = 0; j < agents.size(); ++j) {
     for (int k : agents[j].neighbors) {
-      calculate_force(j, k, agents);
+      calculate_force(j, k, agents, si);
     }
   }
 
   // 運動量、位置の更新
   for (auto &p : agents) {
-    p.vx += a * (p.v0_x + p.fx - p.vx) * dt;
-    p.vy += a * (p.v0_y + p.fy - p.vy) * dt;
+    p.vx += si.a * (p.v0_x + p.fx - p.vx) * si.dt;
+    p.vy += si.a * (p.v0_y + p.fy - p.vy) * si.dt;
 
-    p.x += p.vx * dt;
-    p.y += p.vy * dt;
+    p.x += p.vx * si.dt;
+    p.y += p.vy * si.dt;
 
     // 周期境界条件
-    if (p.x < 0) p.x += LX;
-    if (p.x >= LX) p.x -= LX;
-    if (p.y < 0) p.y += LY;
-    if (p.y >= LY) p.y -= LY;
+    if (p.x < 0) p.x += si.LX;
+    if (p.x >= si.LX) p.x -= si.LX;
+    if (p.y < 0) p.y += si.LY;
+    if (p.y >= si.LY) p.y -= si.LY;
   }
 }
 
 // Pythonにあとで渡すためのYAMLファイルを作成
-void save_yaml(const std::string &filename = "conf.yaml") {
+void save_yaml(SimulationInfo &si, const std::string &filename = "conf.yaml") {
   std::ofstream ofs(filename);
   if (!ofs) {
     std::cerr << "Error: Failed to open " << filename << " for writing." << std::endl;
@@ -194,11 +193,12 @@ void save_yaml(const std::string &filename = "conf.yaml") {
   }
 
   ofs << std::fixed << std::setprecision(6);
-  ofs << "LX: " << LX << "\n";
-  ofs << "LY: " << LY << "\n";
+  ofs << "LX: " << si.LX << "\n";
+  ofs << "LY: " << si.LY << "\n";
 }
 
 void simulation() {
+  SimulationInfo si;
   const int seed = 12346;
   std::mt19937 rng(seed);
   std::vector<People> agents;
@@ -206,17 +206,17 @@ void simulation() {
   std::stringstream ss_stgraph; // 時空図用
 
   const double r = 1.3;
-  initialize_agents_triangular_lattice(agents, LX, LY, r, rng);
-  std::cout << "# " << LX << " " << LY << agents.size() << std::endl;
+  initialize_agents_triangular_lattice(agents, si, r, rng);
+  std::cout << "# " << si.LX << " " << si.LY << agents.size() << std::endl;
   write_agent_frame(agents);
   for (int i = 0; i < 400; i++) {
-    step_simulation(agents);
+    step_simulation(agents, si);
     write_agent_frame(agents);
     for (auto &a : agents) {
-      ss_stgraph << i * dt << " " << a.x << std::endl;
+      ss_stgraph << i * si.dt << " " << a.x << std::endl;
     }
   }
-  save_yaml();
+  save_yaml(si);
 
   // 時空図の保存
   std::ofstream ofs("spatio_temporal.dat");
